ea0d224a755f5d8a62a56de946e88233b7f418bb
Juan Francisco Cisneros <juanfrancistm2011@icloud.com>
2024-01-28 12:20:54 -0500

    Refactored Controller.java to initialize mappers and verify their states.

--- src/Controller.java
+++ src/Controller.java
@@ -1,87 +1,75 @@
 import java.sql.Struct;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Queue;
 
-public class Controller implements Runnable{
-    public  TxtManager txtManager = new TxtManager();
+public class Controller implements Runnable {
+    public TxtManager txtManager = new TxtManager();
     public Queue<String> available_chunks;
-    public Queue<String> available_mapped_chunks;
+    public Queue<String> available_mapped_chunks = new LinkedList<>();;
     public ArrayList<Mapper> mappers = new ArrayList<Mapper>();
 
+    public Boolean map_finsh = false;
 
-
-
-    public Controller(){
+    public Controller() {
         this.available_chunks = txtManager.get_chunks_name();
     }
-
 
     @Override
     public void run() {
         init_mappers();
 
-        while (!available_chunks.isEmpty() ){
-            verify_state_mappers();
-        }
-
-        while (true){
-            if (all_mappers_finished()){
-                break;
+        while (true) {
+            if (!available_chunks.isEmpty()) {
+                verify_state_mappers();
+                if (available_chunks.size() == 0) {
+                    map_finsh = true;
+                }
             }
-            break;
+
+            // Check if all mappers have finished and if it's time to start the reducers
+            if (map_finsh && all_mappers_finished() && available_mapped_chunks.isEmpty()) {
+                System.out.println("Map finished");
+                init_reducers();
+                break; // Exit the loop once the reducers have started
+            }
         }
-        init_reducers();
-
-
-
-
-
-
 
     }
 
-
-    void init_mappers(){
-        //init four mappers
+    void init_mappers() {
+        // init four mappers
         for (int i = 0; i < 4; i++) {
-            Mapper mapper = new Mapper(i,this.available_chunks.remove(), false);
+            Mapper mapper = new Mapper(i, this.available_chunks.remove(), false);
             this.mappers.add(mapper);
             Thread mapperThread = new Thread(mapper);
             mapperThread.start();
-            System.out.println("Mapper "+i+" started");
+            System.out.println("Mapper " + i + " started");
         }
     }
 
-    void verify_state_mappers(){
+    void verify_state_mappers() {
         for (Mapper mapper : this.mappers) {
-            if (!mapper.is_working){
-                //if the mapper is not working, we need to assign another chunk
+            if (!mapper.is_working) {
+                // if the mapper is not working, we need to assign another chunk
                 mapper.is_working = true;
                 mapper.assign_chunk(this.available_chunks.remove());
             }
         }
     }
 
-    boolean all_mappers_finished(){
-        ArrayList<Boolean> all_mappers_finished = new ArrayList<Boolean>();
+    boolean all_mappers_finished() {
         for (Mapper mapper : this.mappers) {
-            if (mapper.is_working){
-                all_mappers_finished.add(true);
-            }
-            else {
-                all_mappers_finished.add(false);
+            if (mapper.is_working) {
+                return false; // Return false as soon as we find a mapper that is still working
             }
         }
-        if (all_mappers_finished.contains(true)){
-            return true;
-        }
-        return false;
+        return true; // Only return true if we've checked all mappers and none of them are working
     }
 
-    void init_reducers(){
+    void init_reducers() {
         available_mapped_chunks = txtManager.get_mapper_name();
         System.out.println(available_mapped_chunks);
     }
-
 
 }
