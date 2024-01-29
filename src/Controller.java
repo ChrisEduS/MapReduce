f8861bb54cb6f8f7452ece79c75ce0ee7d7eab60
Juan Francisco Cisneros <juanfrancistm2011@icloud.com>
2024-01-28 19:22:21 -0500

    Add option to fail mappers and combiners, and implement restart functionality

--- src/Controller.java
+++ src/Controller.java
@@ -1,8 +1,13 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
+
+import static java.lang.Thread.sleep;
 
 public class Controller implements Runnable {
+
+
     public TxtManager txtManager = new TxtManager();
 
     public Queue<String> available_chunks;
@@ -16,14 +21,43 @@ public class Controller implements Runnable {
     public ArrayList<Thread> combiners_threads = new ArrayList<Thread>();
     public Boolean combine_finish = false;
 
-    public Controller() {
+    public int number_of_mappers;
+    public int number_of_combiners;
+    public int mappers_fail;
+    public int combiners_fail;
+
+
+    public Controller(int number_of_mappers, int number_of_combiners, int mappers_fail, int combiners_fail) {
         this.available_chunks = txtManager.get_chunks_name();
+        this.number_of_mappers = number_of_mappers;
+        this.number_of_combiners = number_of_combiners;
+        this.mappers_fail = mappers_fail;
+        this.combiners_fail = combiners_fail;
     }
 
     @Override
     public void run() {
-        init_mappers();
+        try {
+            init_mappers(number_of_mappers, mappers_fail);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    sleep(4000); // Wait 4 seconds
+                    Mapper restart_mapper = mappers.get(mappers_fail);
+                    Thread restart_mapper_thread = new Thread(restart_mapper);
+                    mappers_threads.set(mappers_fail, restart_mapper_thread);
+                    restart_mapper_thread.start();
+                    System.out.println("Mapper " + mappers_fail + " Restarted Successfully!!!!");
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
         while (true) {
             if (!available_chunks.isEmpty()) {
                 verify_state_mappers();
@@ -35,7 +69,23 @@ public class Controller implements Runnable {
             // Check if all mappers have finished and if it's time to start the combiners
             if (map_finsh && all_mappers_finished() && available_mapped_chunks.isEmpty()) {
                 System.out.println("Map finished");
-                init_combiners();
+                init_combiners(number_of_combiners, combiners_fail);
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            sleep(4000); // Wait 5 seconds
+                            Combiner restart_combiner = combiners.get(combiners_fail);
+                            Thread restart_combiner_thread = new Thread(restart_combiner);
+                            combiners_threads.set(combiners_fail, restart_combiner_thread);
+                            restart_combiner_thread.start();
+                            System.out.println("Combiner " + combiners_fail + " Restarted Successfully!!!!");
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }).run();
 
                 while (true){
                     if (!available_mapped_chunks.isEmpty()) {
@@ -63,15 +113,19 @@ public class Controller implements Runnable {
 
     }
 
-    void init_mappers() {
+    void init_mappers(int number_of_mappers, int mappers_fail) throws InterruptedException {
         // init four mappers
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < number_of_mappers; i++) {
             Mapper mapper = new Mapper(i, this.available_chunks.remove(), false);
             this.mappers.add(mapper);
             Thread mapperThread = new Thread(mapper);
             this.mappers_threads.add(mapperThread);
-            mapperThread.start();
             System.out.println("Mapper " + i + " started");
+            mapperThread.start();
+        }
+        if (mappers_fail  >= 0 && mappers_threads.get(mappers_fail).isAlive()) {
+            mappers_threads.get(mappers_fail).stop();
+            System.out.println("Mapper " + mappers_fail + " FAILED, waiting for restart...");
         }
     }
 
@@ -104,16 +158,19 @@ public class Controller implements Runnable {
         return true;
     }
 
-    void init_combiners() {
+    void init_combiners(int number_of_combiners, int combiners_fail) {
         available_mapped_chunks = txtManager.get_mapper_name();
-        
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < number_of_combiners; i++) {
             Combiner combiner = new Combiner(i, this.available_mapped_chunks.remove());
             this.combiners.add(combiner);
             Thread combinerThread = new Thread(combiner);
             this.combiners_threads.add(combinerThread);
             System.out.println("Combiner " + i + " started");
             combinerThread.start();
+        }
+        if (combiners_fail  >= 0 && combiners_threads.get(combiners_fail).isAlive()) {
+            combiners_threads.get(combiners_fail).stop();
+            System.out.println("Combiner " + combiners_fail + " FAILED, waiting for restart...");
         }
     }
 
@@ -124,6 +181,17 @@ public class Controller implements Runnable {
             }
         }
         return true;
+    }
+
+    void ask_to_stop() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want to stop a Thread? (1,2,3,4,5,6,7,8 or 0 to continue)");
+        int option = scanner.nextInt();
+        if (option == 0) {
+            return;
+        } else {
+            mappers_threads.get(option - 1).stop();
+        }
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import static java.lang.Thread.sleep;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
@@ -13,8 +12,60 @@ public class Main {
 
 
 
+
+
+        //Ask for the number of mappers and combiners
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of mappers: ");
+        int number_of_mappers = scanner.nextInt();
+        System.out.println("Enter the number of combiners: ");
+        int number_of_combiners = scanner.nextInt();
+
+        int mappers_fail = 0;
+        int combiners_fail = 0;
+
+        //fail?
+        String fail = "";
+        System.out.println("Do you want to fail a mapper or combiner? (y/n)");
+        fail = scanner.next();
+        switch (fail){
+            case "y":
+                System.out.println("Which one? (mapper/combiner/both)");
+                String which_one = scanner.next();
+                switch (which_one){
+                    case "mapper":
+                        int upper = number_of_mappers - 1;
+                        System.out.println("Which Mapper? (0 - " + upper + ")");
+                        mappers_fail = scanner.nextInt();
+                        break;
+                    case "combiner":
+                        int upper2 = number_of_combiners - 1;
+                        System.out.println("Which Combiner? (0 - " + upper2 + ")");
+                        combiners_fail = scanner.nextInt();
+                        break;
+                    case "both":
+                        int upper3 = number_of_mappers - 1;
+                        int upper4 = number_of_combiners - 1;
+                        System.out.println("Which Mapper? (0 - " + upper3 + ")");
+                        mappers_fail = scanner.nextInt();
+                        System.out.println("Which Combiner? (0 - " + upper4 + ")");
+                        combiners_fail = scanner.nextInt();
+                        break;
+                    default:
+                        System.out.println("Invalid option");
+                        break;
+                }
+                break;
+            case "n":
+                break;
+        }
+
+
+
+
+
         //Init Controller
-        Controller controller = new Controller();
+        Controller controller = new Controller(number_of_mappers, number_of_combiners, mappers_fail, combiners_fail);
         Thread controllerThread = new Thread(controller);
         controllerThread.start();
 
